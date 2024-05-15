384b75c960d4c6030e18ce6e782ad251307266f5
olegoks <69327446+olegoks@users.noreply.github.com>
2024-05-15 03:16:27 +0300

    Test.sh script updated to stop commit procedure if unit tests were not passed.

--- Scripts/Test.sh
+++ Scripts/Test.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 set -x
 cd ../
+
 cd ./Build
-ctest -C Debug
+
+rm ./Testing/Temporary/LastTestsFailed.log
+
+ctest --output-on-failure -C Debug
+
+# value=`cat ./Testing/Temporary/LastTestsFailed.log`
+# echo "$value"
+
+
+# read -p "Press enter to continue"
+if [ -e ./Testing/Temporary/LastTestsFailed.log ]
+then
+    echo "UNIT TESTS WERE NOT PASSED!" 
+    exit -1
+fi
+
 cd ..
 cd Scripts/
+
