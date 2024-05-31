766e13b736ee950b223c0f6c769279041b06efd1
olegoks <oleg.oksenchuk@mail.ru>
2024-05-31 01:00:26 +0100

    Scripts upgraded.

--- Scripts/LoadDependencies.sh
+++ Scripts/LoadDependencies.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+set -x
+cd ../
+git clone https://github.com/microsoft/vcpkg.git
+cd ./vcpkg
+./bootstrap-vcpkg.bat
+echo $(pwd)
+./vcpkg.exe install glfw3
+./vcpkg.exe install vulkan
+./vcpkg.exe install imgui
+./vcpkg.exe install lua
+./vcpkg.exe install luabridge3
+./vcpkg.exe install glslang
+./vcpkg.exe install boost
+./vcpkg.exe install gtest
+./vcpkg.exe install tinyobjloader
+cd ../Scripts/
 
-vcpkg install glfw3
-vcpkg install vulkan
-vcpkg install imgui
-vcpkg install lua
-vcpkg install luabridge3
-vcpkg install glslang
