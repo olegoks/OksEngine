0ab27d4a759d5b2e18dce49d8f13596f527642fe
olegoks <69327446+olegoks@users.noreply.github.com>
2024-06-19 19:23:13 +0300

    Basic realization of control tool

--- Tools/Control/Sources/Control.py
+++ Tools/Control/Sources/Control.py
@@ -1,21 +1,20 @@
-# подключаем библиотеки
-import PySimpleGUI as sg
-import random
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
-# что будет внутри окна
-# первым описываем кнопку и сразу указываем размер шрифта
-layout = [[sg.Button('Новое число',enable_events=True, key='-FUNCTION-', font='Helvetica 16')],
-        # затем делаем текст
-        [sg.Text('Результат:', size=(25, 1), key='-text-', font='Helvetica 16')]]
-# рисуем окно
-window = sg.Window('Генератор случайных чисел', layout, size=(350,100))
-# запускаем основной бесконечный цикл
-while True:
-    # получаем события, произошедшие в окне
-    event, values = window.read()
-    # если нажали на крестик
-    if event in (sg.WIN_CLOSED, 'Exit'):
-        # выходим из цикла
-        break
-# закрываем окно и освобождаем используемые ресурсы
-window.close()
+import sys # Только для доступа к аргументам командной строки
+
+from MainWindow import MainWindow
+# Приложению нужен один (и только один) экземпляр QApplication.
+# Передаём sys.argv, чтобы разрешить аргументы командной строки для приложения.
+# Если не будете использовать аргументы командной строки, QApplication([]) тоже работает
+app = QApplication(sys.argv)
+
+# Создаём виджет Qt — окно.
+window = MainWindow()
+window.show()  # Важно: окно по умолчанию скрыто.
+
+# Запускаем цикл событий.
+app.exec()
+
+
+# Приложение не доберётся сюда, пока вы не выйдете и цикл
+# событий не остановится.
