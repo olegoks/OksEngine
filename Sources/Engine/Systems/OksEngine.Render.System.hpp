658022a4bf633838fd9ae9665562cd4b3c06f205
olegoks <69327446+olegoks@users.noreply.github.com>
2024-06-27 14:19:11 +0300

    Refactoring(Compiled)

---
+++ Sources/Engine/Systems/OksEngine.Render.System.hpp
@@ -0,0 +1,32 @@
+
+#include <ECS.hpp>
+#include <OksEngine.Context.hpp>
+#include <OksEngine.RenderableGeometry.hpp>
+
+namespace OksEngine {
+
+	class RenderSystem : public ECS::System {
+	public:
+
+		RenderSystem(Context& context) noexcept :
+			context_{ context } { }
+
+	public:
+
+		virtual void Update(ECS::World* world, ECS::Entity::Id entityId) const override {
+			auto* immutableRenderGeometry = world->GetComponent<ImmutableRenderGeometry>(entityId);
+			if (immutableRenderGeometry == nullptr) return;
+			//context_.GetRenderSubsystem()->
+
+		}
+
+	private:
+
+		virtual Common::TypeId GetTypeId() const noexcept override {
+			return Common::TypeInfo<RenderSystem>().GetId();
+		}
+
+		Context& context_;
+	};
+
+}
