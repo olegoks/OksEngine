826fa3aab9708b52b6158b9039c032ebecaf02b8
olegoks <69327446+olegoks@users.noreply.github.com>
2024-07-01 03:07:00 +0300

    Attempt to fix camera

--- Sources/Scripts/Camera.lua
+++ Sources/Scripts/Camera.lua
@@ -9,6 +9,9 @@ function Camera:New()
     Camera.Counter = 0;
     Camera.WPressed = false
     Camera.SPressed = false
+    Camera.yaw = -90.0
+    Camera.fov = 45.0
+    Camera.pitch = 0.0
     function Camera:IncreaseCounter()
         self.Counter = self.Counter + 1;
         --print(self.Counter)
@@ -27,14 +30,14 @@ function CameraUpdater:Update(Camera, deltaMs)
     position = Camera:GetComponent("Position")
     cameraComp = Camera:GetComponent("Camera")
     if Camera.WPressed then
-        position:SetX(position:GetX() + cameraComp:GetDirectionX() * 0.01)
-        position:SetY(position:GetY() + cameraComp:GetDirectionY() * 0.01)
-        position:SetZ(position:GetZ() + cameraComp:GetDirectionZ() * 0.01)
+        position:SetX(position:GetX() + cameraComp:GetDirectionX() * 0.1)
+        position:SetY(position:GetY() + cameraComp:GetDirectionY() * 0.1)
+        position:SetZ(position:GetZ() + cameraComp:GetDirectionZ() * 0.1)
     end
     if Camera.SPressed then
-        position:SetX(position:GetX() - cameraComp:GetDirectionX() * 0.01)
-        position:SetY(position:GetY() - cameraComp:GetDirectionY() * 0.01)
-        position:SetZ(position:GetZ() - cameraComp:GetDirectionZ() * 0.01)
+        position:SetX(position:GetX() - cameraComp:GetDirectionX() * 0.1)
+        position:SetY(position:GetY() - cameraComp:GetDirectionY() * 0.1)
+        position:SetZ(position:GetZ() - cameraComp:GetDirectionZ() * 0.1)
     end
 end
 
@@ -62,6 +65,30 @@ function CameraInputProcessor:ProcessInput(Camera, Key, Event, offsetX, offsetY)
         end
     end 
     
+    offsetX = offsetX 
+    offsetY = offsetY 
+
+    Camera.yaw = Camera.yaw + offsetX;
+    Camera.pitch =  Camera.pitch + offsetY;
+
+    if Camera.pitch > 89.0 then
+        Camera.pitch = 89.0
+    elseif Camera.pitch < -89.0 then
+        Camera.pitch = -89.0
+    end 
+
+    local frontX = math.cos(math.rad(Camera.pitch)) *  math.cos(math.rad(Camera.yaw))
+    local frontY = math.sin(math.rad(Camera.pitch))
+    local frontZ = math.cos(math.rad(Camera.pitch)) * math.sin(math.rad(Camera.yaw))
+    --local dirX = cameraComp:GetDirectionX()
+    cameraComp:SetDirectionX(frontX)
+
+    --local dirY = cameraComp:GetDirectionY()
+    cameraComp:SetDirectionY(frontY)
+
+    --local dirZ = cameraComp:GetDirectionZ()
+    cameraComp:SetDirectionZ(frontZ)
+    print("Success")
 end
 
 
