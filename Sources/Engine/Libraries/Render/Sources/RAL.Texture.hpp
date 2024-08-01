0baca26bb858726ecdfa581289753fa84714c6c0
olegoks <69327446+olegoks@users.noreply.github.com>
2024-08-01 18:09:22 +0300

    Refactoring

--- Sources/Engine/Libraries/Render/Sources/RAL.Texture.hpp
+++ Sources/Engine/Libraries/Render/Sources/RAL.Texture.hpp
@@ -1,9 +1,26 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <RAL.Common.hpp>
+#include <Math.Common.hpp>
 
 namespace RAL {
 
 	class Texture {
 	public:
+
+		struct CreateInfo {
+			std::string name_;
+			std::vector<Color4b> pixels_;
+			glm::u32vec2 size_{ 0, 0 };
+		};
+
+		Texture(const CreateInfo& createInfo) noexcept : createInfo_{ createInfo } {}
+
 	private:
+		CreateInfo createInfo_;
 	};
 
 }
