39c7f205aa65e02fa02cd3cc19496c12d6849a51
olegoks <69327446+olegoks@users.noreply.github.com>
2024-08-20 18:23:26 +0300

    Refactoring

--- Sources/Engine/Render/OksEngine.ImmutableRenderGeometry.hpp
+++ Sources/Engine/Render/OksEngine.ImmutableRenderGeometry.hpp
@@ -13,24 +13,29 @@ namespace OksEngine {
 
 		}
 
-		ImmutableRenderGeometry(Context*, float x, float y, float z, std::string obj, std::string mtl, const std::vector<std::string>& textures) : ECSComponent{ nullptr } {
+		ImmutableRenderGeometry(
+			std::string objFileName,
+			std::string mtlFileName,
+			const std::vector<std::string>& textures) :
+			ECSComponent{ nullptr },
+			modelObjFileName_{ objFileName },
+			modelMtlFileName_{ mtlFileName },
+			textures_{ textures } {}
 
-		}
+		//void Rotate(float angle, float x, float y, float z) {
+		//	modelMatrix_ = glm::rotate(modelMatrix_, angle, { x, y, z });
+		//}
 
-		void Rotate(float angle, float x, float y, float z) {
-			modelMatrix_ = glm::rotate(modelMatrix_, angle, { x, y, z });
-		}
+		//void SetTransform(const glm::mat4& transform) {
+		//	/*if(modelObjFileName_ == "GrassBlock.obj" && Math::IsEqual(transform[3][1], 0.0f)) {
+		//		__debugbreak();
+		//	}*/
+		//	modelMatrix_ = transform;
+		//}
 
-		void SetTransform(const glm::mat4& transform) {
-			/*if(modelObjFileName_ == "GrassBlock.obj" && Math::IsEqual(transform[3][1], 0.0f)) {
-				__debugbreak();
-			}*/
-			modelMatrix_ = transform;
-		}
-
-		const glm::mat4& GetTransform() {
-			return modelMatrix_;
-		}
+		//const glm::mat4& GetTransform() {
+		//	return modelMatrix_;
+		//}
 
 		void SetId(Common::Index id) {
 			modelId_ = id;
@@ -40,23 +45,11 @@ namespace OksEngine {
 			return modelId_;
 		}
 
-		ImmutableRenderGeometry(
-			Context* context,
-			const glm::mat4& modelMatrix,
-			std::string objFileName,
-			std::string mtlFileName,
-			const std::vector<std::string>& textures) :
-			ECSComponent{ context },
-			modelMatrix_{ modelMatrix },
-			modelObjFileName_{ objFileName },
-			modelMtlFileName_{ mtlFileName },
-			textures_{ textures } {}
 		std::string modelObjFileName_ = "";
 		std::string modelMtlFileName_ = "";
 		std::vector<std::string> textures_;
 	private:
 		Common::Index modelId_ = Common::Limits<Common::Index>::Max();
-		glm::mat4 modelMatrix_ = glm::identity<glm::mat4>();
 	};
 
 
