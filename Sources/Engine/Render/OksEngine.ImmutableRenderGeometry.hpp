7e630f1e8fc4a59224b0fd3ee9271d2e3363400e
olegoks <69327446+olegoks@users.noreply.github.com>
2024-08-22 18:08:47 +0300

    Merge

--- Sources/Engine/Render/OksEngine.ImmutableRenderGeometry.hpp
+++ Sources/Engine/Render/OksEngine.ImmutableRenderGeometry.hpp
@@ -15,9 +15,8 @@ namespace OksEngine {
 		ImmutableRenderGeometry(const std::string& meshTag) :
 			ECSComponent{ nullptr } {}
 
-<<<<<<< HEAD
 		const std::string meshTag_;
-=======
+
 		ImmutableRenderGeometry(
 			std::string objFileName,
 			std::string mtlFileName,
@@ -27,35 +26,9 @@ namespace OksEngine {
 			modelMtlFileName_{ mtlFileName },
 			textures_{ textures } {}
 
-		//void Rotate(float angle, float x, float y, float z) {
-		//	modelMatrix_ = glm::rotate(modelMatrix_, angle, { x, y, z });
-		//}
-
-		//void SetTransform(const glm::mat4& transform) {
-		//	/*if(modelObjFileName_ == "GrassBlock.obj" && Math::IsEqual(transform[3][1], 0.0f)) {
-		//		__debugbreak();
-		//	}*/
-		//	modelMatrix_ = transform;
-		//}
-
-		//const glm::mat4& GetTransform() {
-		//	return modelMatrix_;
-		//}
-
-		void SetId(Common::Index id) {
-			modelId_ = id;
-		}
-
-		Common::Index GetId() {
-			return modelId_;
-		}
-
 		std::string modelObjFileName_ = "";
 		std::string modelMtlFileName_ = "";
 		std::vector<std::string> textures_;
-	private:
-		Common::Index modelId_ = Common::Limits<Common::Index>::Max();
->>>>>>> 39c7f205aa65e02fa02cd3cc19496c12d6849a51
 	};
 
 
