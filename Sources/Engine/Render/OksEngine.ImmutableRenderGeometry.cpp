fc051295b2131528b4a9e8f58021f4e47f404e7b
olegoks <69327446+olegoks@users.noreply.github.com>
2024-08-27 16:57:14 +0300

    refactoring

--- Sources/Engine/Render/OksEngine.ImmutableRenderGeometry.cpp
+++ Sources/Engine/Render/OksEngine.ImmutableRenderGeometry.cpp
@@ -1,9 +1,13 @@
 #include <Render/OksEngine.ImmutableRenderGeometry.hpp>
 
-ImmutableRenderGeometry::ImmutableRenderGeometry() : ECSComponent{ nullptr }{
+namespace OksEngine {
+
+	ImmutableRenderGeometry::ImmutableRenderGeometry() : ECSComponent{ nullptr } {
+
+	}
+
+	ImmutableRenderGeometry::ImmutableRenderGeometry(const std::string& meshTag) :
+		ECSComponent{ nullptr },
+		meshTag_{ meshTag } {}
 
 }
-
-ImmutableRenderGeometry::ImmutableRenderGeometry(const std::string& meshTag) :
-	ECSComponent{ nullptr },
-	meshTag_{ meshTag } {}
