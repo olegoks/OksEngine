41c22fb98b8ffb740d90e862bc411b98b2628d97
olegoks <69327446+olegoks@users.noreply.github.com>
2024-08-27 17:34:02 +0300

    Fixed compilation

--- Sources/Engine/Render/OksEngine.ImmutableRenderGeometry.cpp
+++ Sources/Engine/Render/OksEngine.ImmutableRenderGeometry.cpp
@@ -1,5 +1,7 @@
 #include <Render/OksEngine.ImmutableRenderGeometry.hpp>
 
+#include <Common/OksEngine.Position.hpp>
+
 namespace OksEngine {
 
 	ImmutableRenderGeometry::ImmutableRenderGeometry() : ECSComponent{ nullptr } {
@@ -9,5 +11,10 @@ namespace OksEngine {
 	ImmutableRenderGeometry::ImmutableRenderGeometry(const std::string& meshTag) :
 		ECSComponent{ nullptr },
 		meshTag_{ meshTag } {}
+
+
+	std::pair<ECS::Entity::Filter, ECS::Entity::Filter> RenderMesh::GetFilter() const noexcept {
+		return { ECS::Entity::Filter{}.Include<Mesh>().Include<ImmutableRenderGeometry>().Include<Position>(), ECS::Entity::Filter{}.ExcludeAll() };
+	}
 
 }
