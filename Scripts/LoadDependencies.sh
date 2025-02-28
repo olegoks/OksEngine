fd5f1e50880ac5b061dc3768655f5b39ac140b66
olegoks <oleg.oksenchuk@mail.ru>
2025-02-28 15:36:41 +0300

    Remove vcpkg boost package loading

--- Scripts/LoadDependencies.sh
+++ Scripts/LoadDependencies.sh
@@ -20,7 +20,6 @@ echo $(pwd)
 ./vcpkg.exe install lua
 ./vcpkg.exe install luabridge3
 ./vcpkg.exe install glslang
-./vcpkg.exe install boost-thread
 ./vcpkg.exe install gtest
 ./vcpkg.exe install stb
 ./vcpkg.exe install loguru
