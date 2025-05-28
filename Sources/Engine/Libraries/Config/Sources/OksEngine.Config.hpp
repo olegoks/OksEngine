bfd285f81b872e64166bf61197381e4d92937903
olegoks <oleg.oksenchuk@mail.ru>
2025-05-28 16:27:48 +0300

    Rename class to avoid names collision with Config ecs component

--- Sources/Engine/Libraries/Config/Sources/OksEngine.Config.hpp
+++ Sources/Engine/Libraries/Config/Sources/OksEngine.Config.hpp
@@ -5,27 +5,27 @@
 
 namespace OksEngine {
 
-	//struct Config {
-	//	std::filesystem::path resourcesPath_;
+	struct ConfigFile {
+		std::filesystem::path resourcesPath_;
 
-	//	Config(Config&& moveConfig) : 
-	//		context_{ std::move(moveConfig.context_) } {
+		ConfigFile(ConfigFile&& moveConfig) : 
+			context_{ std::move(moveConfig.context_) } {
 
-	//	}
+		}
 
-	//	Config(const std::string& text) {
-	//		::Lua::Script script{ text };
-	//		context_.LoadScript(script);
-	//	}
+		ConfigFile(const std::string& text) {
+			::Lua::Script script{ text };
+			context_.LoadScript(script);
+		}
 
-	//	template<class Type>
-	//	[[nodiscard]]
-	//	Type GetValueAs(const char* valuePath) const noexcept {
-	//		return context_.GetGlobalAs<Type>(valuePath);
-	//	}
+		template<class Type>
+		[[nodiscard]]
+		Type GetValueAs(const char* valuePath) const noexcept {
+			return context_.GetGlobalAs<Type>(valuePath);
+		}
 
-	//private:
-	//	::Lua::Context context_;
-	//};
+	private:
+		::Lua::Context context_;
+	};
 
 }
