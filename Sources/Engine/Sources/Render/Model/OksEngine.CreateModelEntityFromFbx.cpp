f3d78c104ebfb37eb28015d82532b21c130e60f2
olegoks <oleg.oksenchuk@mail.ru>
2025-09-02 23:21:12 +0300

    Remove unused code

--- Sources/Engine/Sources/Render/Model/OksEngine.CreateModelEntityFromFbx.cpp
+++
@@ -1,5 +0,0 @@
-#include <Render\Model\auto_OksEngine.CreateModelEntityFromFbx.hpp>
-
-namespace OksEngine {
-void Update(ECS2::Entity::Id entityId, const FbxEntity* fbxEntity) {};
-}
--- Sources/Engine/Sources/Render/Model/OksEngine.CreateModelEntityFromObjMtl.cpp
+++
@@ -1,6 +0,0 @@
-#include <Render\Model\auto_OksEngine.CreateModelEntityFromObjMtl.hpp>
-
-namespace OksEngine {
-void Update(ECS2::Entity::Id entityId, const ObjEntity* objEntity,
-            const MtlEntity* mtlEntity) {};
-}
