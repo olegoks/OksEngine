65ad79c9008cf50de4346a069ecf7abc7cf2b7bd
olegoks <oleg.oksenchuk@mail.ru>
2025-09-02 22:58:25 +0300

    Removed old animation realization

--- Sources/Engine/Sources/Animation/OksEngine.CreateAnimationState.hpp
+++
@@ -1,6 +0,0 @@
-#include <Animation\OksEngine.CreateAnimationState.cpp>
-
-namespace OksEngine {
-void Update(ECS2::Entity::Id entityId, const Animation* animation,
-            const StartAnimation* startAnimation) {};
-}
