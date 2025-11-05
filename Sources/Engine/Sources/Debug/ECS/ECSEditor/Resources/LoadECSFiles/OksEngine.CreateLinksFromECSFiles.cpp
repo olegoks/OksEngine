dc3cd37ddea417a3a9e9d77dfb8edc101f1b8de1
olegoks <oleg.oksenchuk@mail.ru>
2025-11-06 00:33:45 +0300

    ECS Editor inspector

--- Sources/Engine/Sources/Debug/ECS/ECSEditor/Resources/LoadECSFiles/OksEngine.CreateLinksFromECSFiles.cpp
+++ Sources/Engine/Sources/Debug/ECS/ECSEditor/Resources/LoadECSFiles/OksEngine.CreateLinksFromECSFiles.cpp
@@ -2,18 +2,27 @@
 
 namespace OksEngine {
 	void CreateLinksFromECSFiles::Update(
-		ECS2::Entity::Id entity0id, const ImGUI::State* imGuiState0,
+		ECS2::Entity::Id entity0id, 
+		const ImGUI::State* imGuiState0,
 		const EditorContext* editorContext0,
 		const ECSEditorWindow* eCSEditorWindow0,
 		const CreateLinksRequest* createLinksRequest0,
-		ECS2::Entity::Id entity1id, const CallGraphNode* callGraphNode1,
-		const Name* name1, const RunAfter* runAfter1,
-		const RunBefore* runBefore1, const BeforePin* beforePin1,
+
+		ECS2::Entity::Id entity1id, 
+		const CallGraphNode* callGraphNode1,
+		const Name* name1, 
+		const RunAfter* runAfter1,
+		const RunBefore* runBefore1, 
+		const BeforePin* beforePin1,
 		const AfterPin* afterPin1, 
 		
-		ECS2::Entity::Id entity2id, const CallGraphNode* callGraphNode2,
-		const Name* name2, const RunAfter* runAfter2, const RunBefore* runBefore2,
-		const BeforePin* beforePin2, const AfterPin* afterPin2) {
+		ECS2::Entity::Id entity2id, 
+		const CallGraphNode* callGraphNode2,
+		const Name* name2, 
+		const RunAfter* runAfter2,
+		const RunBefore* runBefore2,
+		const BeforePin* beforePin2,
+		const AfterPin* afterPin2) {
 
 
 		if (std::find(
